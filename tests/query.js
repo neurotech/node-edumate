f505c4a6e54348433c7bd3e9993d2e26d745e078
Tim Douglas <neurotech@gmail.com>
2017-03-16 08:24:28 +1100

    Updated tests to match refactoring

--- tests/query.js
+++ tests/query.js
@@ -1,11 +1,8 @@
-'use strict';
+const test = require('tape');
+const camelCase = require('camel-case');
+const edumate = require('../lib/edumate');
 
-var test = require('tape');
-var camelCase = require('camel-case');
-var path = require('path');
-var edumate = require('../lib/edumate');
-
-var config = {
+let config = {
   host: process.env.EDUMATE_HOST,
   port: process.env.EDUMATE_PORT,
   suffix: process.env.EDUMATE_PATH,
@@ -13,56 +10,97 @@ var config = {
   password: process.env.EDUMATE_PASSWORD
 };
 
-var sql = 'SELECT * FROM EDUMATE.academic_year WHERE academic_year = YEAR(current date)';
+let sql = 'SELECT * FROM EDUMATE.academic_year WHERE academic_year = YEAR(current date)';
+let many = [
+  'SELECT \'Example query results 1.\' AS "CAMEL_CASE_COLUMN" FROM SYSIBM.sysdummy1',
+  'SELECT \'Example query results 2.\' AS "CAMEL_CASE_COLUMN" FROM SYSIBM.sysdummy1',
+  'SELECT \'Example query results 3.\' AS "CAMEL_CASE_COLUMN" FROM SYSIBM.sysdummy1'
+];
 
-test('node-edumate - query - config object + sql statment validity', function (t) {
+test('node-edumate - query - config object + sql statement validity', function (t) {
   t.plan(6);
-  t.equal(typeof config.host, 'string', 'db2 host present');
-  t.equal(typeof config.port, 'string', 'db2 port present');
-  t.equal(typeof config.suffix, 'string', 'db2 path present');
-  t.equal(typeof config.username, 'string', 'db2 username present');
-  t.equal(typeof config.password, 'string', 'db2 password present');
-  t.equal(typeof sql, 'string', 'sql statment present');
+  t.equal(typeof config.host, 'string', 'DB2 host present');
+  t.equal(typeof config.port, 'string', 'DB2 port present');
+  t.equal(typeof config.suffix, 'string', 'DB2 path present');
+  t.equal(typeof config.username, 'string', 'DB2 username present');
+  t.equal(typeof config.password, 'string', 'DB2 password present');
+  t.equal(typeof sql, 'string', 'SQL statement present');
 });
 
-test('node-edumate - query - no arguments supplied', function (t) {
+test('node-edumate - open connection to db', function (t) {
   t.plan(1);
-  edumate.query()
-    .then(function (results) {
-      t.equal(results, null);
-    }, function (error) {
-      t.ok(error, 'error because !config');
-    });
+  edumate.open(config, (err, conn) => {
+    if (err) {
+      t.equal(err, null);
+    } else {
+      t.equal(conn.connected, true, 'connected to db');
+    }
+  });
 });
 
-test('node-edumate - query - config and sql supplied', function (t) {
+test('node-edumate - close connection to db', function (t) {
   t.plan(1);
-  edumate.query(config, sql)
-    .then(function (results) {
-      t.equal(results.length, 1, '1 result returned');
-    }, function (error) {
-      t.equal(error, null);
-    });
+  edumate.open(config, (err, conn) => {
+    if (err) {
+      t.equal(err, null);
+    } else {
+      edumate.close(conn, (err) => {
+        if (err) {
+          t.equal(err, null);
+        } else {
+          t.equal(conn.connected, false, 'disconnected from db');
+        }
+      });
+    }
+  });
 });
 
-test('node-edumate - query - clean: true', function (t) {
+test('node-edumate - single query - clean: true', function (t) {
   t.plan(1);
-  edumate.query(config, sql, {clean: true})
-    .then(function (results) {
-      var firstKey = Object.keys(results[0])[0];
+  edumate.query(config, { clean: true }, sql, (err, results) => {
+    if (err) {
+      t.equal(err, null);
+    } else {
+      let firstKey = Object.keys(results[0])[0];
       t.equal(firstKey, camelCase(firstKey), 'first key *is* camelCase');
-    }, function (error) {
-      t.equal(error, null);
-    });
+    }
+  });
 });
 
-test('node-edumate - query - clean: false', function (t) {
+test('node-edumate - single query - clean: false', function (t) {
   t.plan(1);
-  edumate.query(config, sql, {clean: false})
-    .then(function (results) {
-      var firstKey = Object.keys(results[0])[0];
+  edumate.query(config, { clean: false }, sql, (err, results) => {
+    if (err) {
+      t.equal(err, null);
+    } else {
+      let firstKey = Object.keys(results[0])[0];
       t.notEqual(firstKey, camelCase(firstKey), 'first key is *not* camelCase');
-    }, function (error) {
-      t.equal(error, null);
-    });
+    }
+  });
+});
+
+test('node-edumate - multiple queries', function (t) {
+  t.plan(1);
+  edumate.open(config, (err, conn) => {
+    if (err) { t.equal(err, null); }
+    let goal = many.length;
+    let collection = [];
+    let looper = (item) => {
+      if (item) {
+        setTimeout(() => {
+          edumate.queries({ clean: true }, conn, item, (err, results) => {
+            if (err) { t.equal(err, null); }
+            collection.push(results[0]);
+            return looper(many.shift());
+          });
+        }, 500);
+      } else {
+        edumate.close(conn, (err) => {
+          if (err) { t.equal(err, null); }
+          t.equal(collection.length, goal, `${goal} queries executed.`);
+        });
+      }
+    };
+    looper(many.shift());
+  });
 });
